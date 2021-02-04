5aa9c171f1f0fa92a041a28eece2a45ae2b79f74
Andrius Daraškevičius <andrius.daraskevicius@gmail.com>
2021-02-04 23:17:22 +0200

    Typescript setup

---
+++ QueueTickets/ClientApp/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+
+import './custom.css'
+
+
+
+const App: React.FC = props => {
+    return (
+        <div className="App">
+            <p>test test lololol</p>
+        </div>
+    )
+}
+
+export default App
